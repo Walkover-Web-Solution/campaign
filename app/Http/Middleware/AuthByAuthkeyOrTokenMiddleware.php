55cea629a7a27078605b3dc5a9aa34a2f9732629
Prasuk <prasuk@whozzat.com>
2022-03-08 16:34:52 +0530

    Prasuk | wrote middlewares and helper.php

--- app/Http/Middleware/AuthByAuthkeyOrTokenMiddleware.php
+++ app/Http/Middleware/AuthByAuthkeyOrTokenMiddleware.php
@@ -2,11 +2,36 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Company;
+use App\Models\Token;
 use Closure;
+use Illuminate\Cache\RateLimiter;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class AuthByAuthkeyOrTokenMiddleware
 {
+
+    public $limiter;
+    protected $ip;
+
+    public function __construct(RateLimiter $limiter)
+    {
+        $this->limiter = $limiter;
+        if (request()->header('userip')) {
+            $ips = explode(',', request()->header('userip'));
+            $this->ip = $ips[0];
+        } elseif (request()->header('cf-connecting-ip')) {
+            $this->ip = request()->header('cf-connecting-ip');
+        } else {
+            $this->ip = request()->ip();
+        }
+        request()->merge([
+            'ip' => $this->ip
+        ]);
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -16,6 +41,139 @@ class AuthByAuthkeyOrTokenMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+        if (!($request->hasHeader('token') || $request->hasHeader('authorization'))) {
+            throw new \Exception("User not Authorized", 1);
+        }
+
+
+        if ($request->header('authorization')) {
+
+            //decode the authorization key
+            try {
+                $res = JWTDecode(request()->header('authorization'));
+            } catch (\Exception $e) {
+                throw new \Exception("Unauthorized", 1);
+            }
+
+            // get company with re_id and checks if key has company that exists in db table
+            $company = Company::where('ref_id', $res->company->id)->first();
+            if (empty($company)) {
+                throw new \Exception("User not Authorized");
+            }
+
+            //get campaign with slug
+            $campaign = $company->campaigns()->where('slug', $request->slug)->first();
+            if (empty($campaign)) {
+                throw new \Exception("Invalid Campaign", 1);
+            }
+
+            //checks if slug's campaign's id exists in db table
+            if (!$company->campaigns()->where('id', $campaign->id)->exists()) {
+                throw new \Exception("User not Authorized");
+            }
+        } else {
+
+
+            // token validation
+            $token = Token::where('token', $request->header('token'))->first();
+
+            if (empty($token)) {
+                throw new \Exception("User not Authorized");
+            }
+
+            $ip = $this->ip;
+            $whiteListTypeIP = 1;
+            if (!$token->ips()->where('ip', $ip)->where('ip_type_id', $whiteListTypeIP)->exists()) {
+                $this->validateUserIP($token);
+                $this->throttleTokenValidate($token);
+            }
+
+            $campaign = $token->company->campaigns()->where('slug', $request->slug)->first();
+            if (empty($campaign)) {
+                throw new \Exception("Invalid Campaign", 1);
+            }
+            if ($campaign->company_token_id != $token->id) {
+                throw new \Exception("User not Authorized");
+            }
+        }
+
+        $request->merge([
+            'campaign' => $campaign,
+            'company' => $campaign->company
+        ]);
+
+
         return $next($request);
+    }
+
+
+    public function validateUserIP($token)
+    {
+        $ip = $this->ip;
+
+        $whiteListTypeIP = 1;
+        $blackListIPType = 2;
+        $temporaryBlockedIPType = 3;
+        $tokenIP = $token->ips()->where('ip', $ip)->first();
+        if (!empty($tokenIP)) {
+            if ($tokenIP->ip_type_id == $blackListIPType) {
+                throw new \Exception("Your IP is blocked");
+            }
+
+            if ($tokenIP->ip_type_id == $temporaryBlockedIPType) {
+                if (time() < strtotime(ISTToGMT($tokenIP->expires_at))) {
+                    throw new \Exception($ip . " is blocked temporary");
+                } else {
+                    $tokenIP->delete();
+                }
+            }
+        }
+
+        $temp = explode(':', $token->temporary_throttle_limit);
+        $tempThrottleCount = $temp[0];
+        $tempThrottleRange = $temp[1];
+
+        $key = 'temporary_blocked_id:' . $token->id . ':' . $ip;
+        if (Cache::has($key)) {
+            $temporaryCount = Cache::increment($key);
+        } else {
+            $temporaryCount = (int)Cache::put($key, 1, $tempThrottleRange);
+        }
+
+        if ($temporaryCount > $tempThrottleCount) {
+            $ipObj = $token->ips()->where('ip', $ip)->first();
+            //  block  ip temporary
+            if ($ipObj) { //  case of none tye 
+                $ipObj->update([
+                    'ip_type_id' => $temporaryBlockedIPType,
+                    'expires_at' => date('Y-m-d H:i:s', time() + $token->temporary_throttle_time)
+                ]);
+            } else {
+                $token->ips()->create([
+                    'ip' => $ip,
+                    'ip_type_id' => $temporaryBlockedIPType,
+                    'expires_at' => date('Y-m-d H:i:s', time() + $token->temporary_throttle_time)
+                ]);
+            }
+
+            Cache::forget($key); // reset the  counter
+            throw new \Exception($ip . "  blocked  temporarily", 1);
+        }
+    }
+
+
+    public function throttleTokenValidate($token)
+    {
+        $throttleLimit = $token->throttle_limit;
+        $key = 'campaign_run_' . $this->ip;
+        $throttleArr = explode(':', $throttleLimit);
+        $limit = $throttleArr[0];
+        $rate = $throttleArr[1];
+
+        if ($this->limiter->tooManyAttempts($key, $limit)) {
+            throw new ThrottleRequestsException("Too many attempts");
+        }
+
+        $this->limiter->hit($key, $rate);
     }
 }
--- app/Http/Middleware/AuthByJWTOrAuthkeyMiddleware.php
+++ app/Http/Middleware/AuthByJWTOrAuthkeyMiddleware.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Company;
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 
@@ -16,6 +18,74 @@ class AuthByJWTOrAuthkeyMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+        /*
+         * Checks if the reuqest header has authorization or authkey if both not present throw exception
+        */
+        if (empty($request->header('authorization')) && empty($request->header('authkey'))) {
+            throw new \Exception('Unauthorized', 1);
+        }
+
+        if ($request->header('authorization')) {
+
+            /*
+             * decode the authorization key
+             */
+            try {
+                $res = JWTDecode($request->header('authorization'));
+            } catch (\Exception $e) {
+                throw new \Exception('Unauthorized', 1);
+            }
+
+            /*
+             * checks if the key's company id matches with ref_id from the db tabale
+             */
+            $company = Company::where('ref_id', $res->company->id)->first();
+
+            if (empty($company)) {
+                throw new \Exception("User is not Authorized");
+            }
+            if (!empty($res->user)) {
+                $user = $res->user;
+            }
+        } else {
+            //logic for authkey 
+        }
+
+        if (!empty($user)) {
+
+            /**
+             * checks if authorization key has user permissions if exists merge to request
+             */
+            if (isset($res->user->permissions)) {
+                $request->merge(['permissions' => json_decode($user->permissions)]);
+            }
+
+            /*
+             * get user which matches
+             */
+            $user = User::where('ref_id', $user->id)->first();
+            // if (!empty($user)) {
+            $request->merge([
+                'user' => $user
+            ]);
+            // } else {
+            //     throw new \Exception('User doesn\'t exist');
+            // }
+        }
+
+        /*
+         * get campaign using slug coming from request, if found empty throw exception
+         */
+        $campaign = $company->campaigns()->where('slug', $request->slug)->first();
+        if (empty($campaign)) {
+            throw new \Exception('Invalid Campaign', 1);
+        }
+
+        $request->merge([
+            'company' => $company,
+            'campaign' => $campaign
+        ]);
+
         return $next($request);
     }
 }
