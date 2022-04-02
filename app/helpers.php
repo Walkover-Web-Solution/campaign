421abbe4815e2454f50f6a929eaa62ce7784ff26
paaresh-jaisinghani <paresh@whozzat.com>
2022-04-02 12:18:16 +0530

    Paresh | update in wrapper class (helper.php ) to get campaign

--- app/helpers.php
+++ app/helpers.php
@@ -2,6 +2,7 @@
 
 use App\Http\Resources\CustomResource;
 use App\Models\Campaign;
+use App\Models\ChannelType;
 use App\Models\FlowAction;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
@@ -64,11 +65,22 @@ function getCampaign($campid)
     $data->id = $campaign['id'];
     $data->name = $campaign['name'];
     $data->style = $campaign['style'];
-    $data->module_data=$campaign['module_data'];
-    $flow = FlowAction::where('campaign_id',$campid)->get();
-    collect($flow)->map(function($val){
+    $data->module_data = $campaign['module_data'];
+    $data->modules = [];
+    $flow = FlowAction::where('campaign_id', $campid)->get();
 
-        
+    $modules = collect($flow)->map(function ($val, $key) use ($data) {
+        $channel = ChannelType::where('id', $val['channel_id'])->pluck('name')->first();
+        if (empty($data->modules[$channel]))
+            $data->modules[$channel] = [];
+        $temp = new \stdClass();
+        $temp->id = $val['id'];
+        $temp->name = $val['name'];
+        $temp->style = $val['style'];
+        $temp->module_data = $val['module_data'];
+        array_push($data->modules[$channel], $temp);
+        // return $temp;
     });
+    // $data->modules = $modules;
     dd($data);
 }
