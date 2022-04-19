92d0ad160bcb1a7adb130f2b39f9bb5235aacde9
Ravi Soni <ravisoni@walkover.in>
2022-04-19 16:52:16 +0530

    Ravi | removed client table seeder from database seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(ClientTableSeeder::class);
+        // No need to run client seeder for now
+        // $this->call(ClientTableSeeder::class);
         $this->call(ChannelTypeTableSeeder::class);
         $this->call(IPTypeTableSeeder::class);
         $this->call(ConditionTableSeeder::class);
