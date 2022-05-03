7fa2f1c1151c533053d15d02e3d185e786dbf594
jprasuk112 <prasuk@whozzat.com>
2022-05-03 16:45:26 +0530

    Prasuk | Some modification in logic of converting dryRun body to run body for single object of to and mobiles

--- app/Http/Controllers/API/RunCampaignController.php
+++ app/Http/Controllers/API/RunCampaignController.php
@@ -52,7 +52,7 @@ class RunCampaignController extends Controller
             'ip' => request()->ip(),
             'need_validation' => empty($request->need_validation) ? 0 : $request->need_validation
         ];
-      
+
         $campaignLog = $campaign->campaignLogs()->create($logs);
 
         if ($request->filled('data')) {
@@ -80,22 +80,43 @@ class RunCampaignController extends Controller
         $obj->data['sendTo'][0]['bcc'] = [];
         $obj->data['sendTo'][0]['variables'] = [];
 
+        $to = collect($request->data)->where('name', 'to')->first();
+        $mobiles = collect($request->data)->where('name', 'mobiles')->first();
+
+        $toArr = [];
+        $mobilesArr = [];
+        if (!empty($to))
+            $toArr = explode(',', $to['value']);
+        if (!empty($mobiles))
+            $mobilesArr = explode(',', $mobiles['value']);
+
+        // merging to and mobiles into single object
+        if (count($mobilesArr) > count($toArr)) {
+            $maxCount = count($mobilesArr);
+        } else {
+            $maxCount = count($toArr);
+        }
+        for ($i = 0; $i < $maxCount; $i++) {
+            array_push(
+                $obj->data['sendTo'][0]['to'],
+                ['name' => null, 'email' => empty($toArr[$i]) ? null : $toArr[$i], 'mobiles' =>  empty($mobilesArr[$i]) ? null : $mobilesArr[$i]]
+            );
+        }
 
         //convert this body to new run request body
         collect($request->data)->each(function ($ob) use ($obj) {
             $key = $ob['name'];
-            $myArr = explode(',', $ob['value']);
-            collect($myArr)->each(function ($item) use ($key, $obj, $ob) {
-                if (!empty($item))
-                    if ($key == 'mobiles')
-                        array_push($obj->data['sendTo'][0]['to'], ['name' => null, 'email' => null, 'mobiles' => $item]);
-                    else if ($key == 'to' || $key == 'cc' || $key == 'bcc')
+            if ($key == 'cc' || $key == 'bcc') {
+                $myArr = explode(',', $ob['value']);
+                collect($myArr)->each(function ($item) use ($key, $obj, $ob) {
+                    if (!empty($item))
                         array_push($obj->data['sendTo'][0][$key], ['name' => null, 'email' => $item, 'mobiles' => null]);
-                    else {
-                        $value = $ob['value'];
-                        $obj->data['sendTo'][0]['variables'][$key] = $value;
-                    }
-            });
+                });
+            } else if (!($key == 'to' || $key == 'mobiles')) {
+                //for variables
+                $value = $ob['value'];
+                $obj->data['sendTo'][0]['variables'][$key] = $value;
+            }
         });
 
         $request->merge(['data' => $obj->data]);
