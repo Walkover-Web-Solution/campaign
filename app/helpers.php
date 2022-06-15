028d4dc1593602e4cfa77a324f5b390ed01344c4
jprasuk112 <97219996+jprasuk112@users.noreply.github.com>
2022-06-15 14:00:52 +0530

    Prausk | Created a GeneralController for /encode, /failedJobs, /units + Create an api which returns all units which are in use, like for time seconds, minutes, hours etc. (#72)
    Reviewed and approved

--- app/helpers.php
+++ app/helpers.php
@@ -156,3 +156,30 @@ function createNewJob($channel_id, $input, $delay = 0)
 
     $lib->enqueue($queue, $input);
 }
+
+
+function getSeconds($unit, $value)
+{
+    $value = (int)$value;
+
+    switch ($unit) {
+        case "seconds": {
+                return $value * 1;
+            }
+        case "minutes": {
+                return $value * 60;
+            }
+            break;
+        case "hours": {
+                return $value * 3600;
+            }
+            break;
+        case "days": {
+                return $value * 86400;
+            }
+            break;
+        default: {
+                return 0;
+            }
+    }
+}
