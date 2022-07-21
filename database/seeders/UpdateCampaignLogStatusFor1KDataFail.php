ce62a988e0162b693e4e0673c32c8728b0d2f384
Paresh Jaisinghani <97221370+paresh-jaisinghani@users.noreply.github.com>
2022-07-21 19:07:21 +0530

    Paresh | changes in retry API , Fix bugs , add new column as retry_status in campain_log (#100)

--- database/seeders/UpdateCampaignLogStatusFor1KDataFail.php
+++ database/seeders/UpdateCampaignLogStatusFor1KDataFail.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Libs\MongoDBLib;
 use App\Models\ActionLog;
 use App\Models\CampaignLog;
 use App\Models\FailedJob;
@@ -15,12 +16,23 @@ class UpdateCampaignLogStatusFor1KDataFail extends Seeder
      *
      * @return void
      */
+    protected $mongo;
     public function run()
     {
+        if (empty($this->mongo)) {
+            $this->mongo = new MongoDBLib;
+        }
         $campaignLogs = CampaignLog::doesnthave('actionLogs')->get();
         collect($campaignLogs)->map(function ($campaignLog) {
             $failJob = FailedJob::where('uuid', $campaignLog->id)->first();
             $campaignLog->status = 'Error' . (!empty($failJob) ? ' - ' . $failJob->id : '');
+
+            $data = $this->mongo->collection('run_campaign_data')->find([
+                'requestId' => $campaignLog->mongo_uid
+            ]);
+            if (!empty($data)) {
+                $campaignLog->canRetry = true;
+            }
             $campaignLog->save();
         });
     }
